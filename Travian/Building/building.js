4212ecac9aa3b0c422be2003d8d835ebe3513b72
joandwi <joandwi@outlook.com>
2019-01-15 15:42:31 +0100

    Starting to add support to building

---
+++ Travian/Building/building.js
@@ -0,0 +1,13 @@
+class building{
+    constructor() {
+    }
+    getCostForBuildingLevel(level, recursive) {
+        return 0;
+    }
+    getCulturePointForLevel(level) {
+        return 0;
+    }
+    getProductionTimeForLevel(level, mainBuildingLevel){
+        return 0;
+    }
+}
